18ff6ad5697930097cdf7be57c92822d679284ae
simmeringstew <dev.95a2e@simplelogin.com>
2022-11-09 09:52:55 -0500

    Added modal to get users api key

--- src/js/main.js
+++ src/js/main.js
@@ -3,3 +3,24 @@ import '../scss/styles.scss'
 
 // Import all of Bootstrap's JS
 import * as bootstrap from 'bootstrap'
+
+let apiKey = undefined;
+
+// selector for the modal
+const myModal = document.querySelector(".modal");
+const modal = new bootstrap.Modal(myModal);
+
+window.onload = () => {
+    modal.show();
+}
+
+const apiInput = document.querySelector("#apiKeyInput");
+const submit = document.querySelector(".modal-submit");
+submit.addEventListener("click", () => {
+    apiKey = apiInput.value;
+});
+
+const test = document.querySelector(".test");
+test.addEventListener("click", () => {
+    console.log(apiKey);
+});
